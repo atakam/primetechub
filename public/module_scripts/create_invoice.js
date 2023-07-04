58b1f511d1302098400f88590f12cc3adb803af9
austin.takam@sihone.com <austin.takam@sihone.com>
2023-07-04 13:43:52 -0400

    fixing final bugs

--- public/module_scripts/create_invoice.js
+++ public/module_scripts/create_invoice.js
@@ -12,7 +12,7 @@ function update_total() {
 
 		total = parseFloat(total);
 
-		sub_total = total+sub_total;
+		sub_total = Math.floor(total+sub_total);
     });
 	// discount
 	var discount_figure = $('.discount_figure').val();
@@ -42,11 +42,11 @@ function update_total() {
 	var tax_rate = $('.tax_rate').val();
 
 	var gdTotal = parseFloat(sub_total) - parseFloat(discount_amount);
-	var grand_total = parseFloat(gdTotal);
+	var grand_total = Math.floor(parseFloat(gdTotal));
 	jQuery('.sub_total').html(sub_total);
 	jQuery('.items-sub-total').val(sub_total);
-	$('.fgrand_total').val(grand_total.toFixed(2));
-	$('.grand_total').html(grand_total.toFixed(2));
+	$('.fgrand_total').val(grand_total);
+	$('.grand_total').html(grand_total);
 }
 //Update total function ends here.
   // for qty
